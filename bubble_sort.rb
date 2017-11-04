55a567c7c1d2c10b7a7ed452ced3d48677f3ec09
Eric Yang <ericliuyang@gmail.com>
2017-11-04 14:51:06 -0700

    Update bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,8 +1,6 @@
 # Write a function `bubble_sort(arr)` which will sort an array of
 # integers using the "bubble sort"
 # methodology. (http://en.wikipedia.org/wiki/Bubble_sort)
-#
-# Difficulty: 3/5
 
 def bubble_sort(arr)
   sorted = false
