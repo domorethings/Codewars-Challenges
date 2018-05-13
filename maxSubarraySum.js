c7d736901925160ecbd7f97a81fcc0b66b50b7e1
Eric Yang <4650519+domorethings@users.noreply.github.com>
2018-05-13 16:21:36 -0700

    Update maxSubarraySum.js

--- maxSubarraySum.js
+++ maxSubarraySum.js
@@ -1,3 +1,5 @@
+// The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+
 var maxSequence = function(arr) {
 	var currentSum = 0;
 	return arr.reduce((maxSum, number) => {
